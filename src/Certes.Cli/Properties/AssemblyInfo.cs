573c43bb26b2bd3fe5eb2903d49abc6c7a570971
Eddie Lin <fszlin@gmail.com>
2017-03-28 12:05:43 -0400

    update project config for generating nuget package #3

--- src/Certes.Cli/Properties/AssemblyInfo.cs
+++ src/Certes.Cli/Properties/AssemblyInfo.cs
@@ -1,19 +1,4 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Certes.Cli")]
-[assembly: AssemblyTrademark("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("23fd2074-1b0f-495a-831e-7a0d645cdf88")]
-[assembly: AssemblyVersion("1.0")]
--- src/Certes/Properties/AssemblyInfo.cs
+++ src/Certes/Properties/AssemblyInfo.cs
@@ -2,20 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Certes")]
-[assembly: AssemblyTrademark("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("5f6267a0-a6c5-4d1e-b7ff-f8f8d09d8588")]
 [assembly: InternalsVisibleTo("Certes.Tests")]
-[assembly: AssemblyVersion("1.0")]
