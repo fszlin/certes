339aeb260c63195e4012cb37744cbafac7a34fa5
Eddie Lin <fszlin@gmail.com>
2017-03-28 22:22:56 -0400

    update build config

--- src/Certes.Cli/Properties/AssemblyInfo.cs
+++ src/Certes.Cli/Properties/AssemblyInfo.cs
@@ -1,4 +1,3 @@
 ﻿using System.Runtime.InteropServices;
 
-[assembly: ComVisible(false)]
 [assembly: Guid("23fd2074-1b0f-495a-831e-7a0d645cdf88")]
--- src/Certes/Properties/AssemblyInfo.cs
+++ src/Certes/Properties/AssemblyInfo.cs
@@ -1,8 +1,5 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: ComVisible(false)]
 [assembly: Guid("5f6267a0-a6c5-4d1e-b7ff-f8f8d09d8588")]
 [assembly: InternalsVisibleTo("Certes.Tests")]
-[assembly: AssemblyFileVersion("1.0.5.0")]
