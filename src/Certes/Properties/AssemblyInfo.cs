fa581360c62bf24463d0b4079ab163c290c7a143
Eddie Lin <fszlin@gmail.com>
2017-03-28 21:21:30 -0400

    update assembly version

--- src/Certes/Properties/AssemblyInfo.cs
+++ src/Certes/Properties/AssemblyInfo.cs
@@ -18,4 +18,4 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("5f6267a0-a6c5-4d1e-b7ff-f8f8d09d8588")]
 [assembly: InternalsVisibleTo("Certes.Tests")]
-[assembly: AssemblyVersion("1.0")]
+[assembly: AssemblyVersion("1.0.5")]
