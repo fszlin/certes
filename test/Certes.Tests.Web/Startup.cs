0a992eb8415615ec5f6e032aa2e286b7411713e1
Eddie Lin <fszlin@gmail.com>
2017-05-11 15:52:11 -0400

    update badge

--- test/Certes.Tests.Web/Startup.cs
+++ test/Certes.Tests.Web/Startup.cs
@@ -3,6 +3,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Certes.Tests.Web
 {
@@ -13,7 +17,7 @@ namespace Certes.Tests.Web
         public void ConfigureServices(IServiceCollection services)
         {
         }
-        
+
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole();
@@ -33,6 +37,39 @@ namespace Certes.Tests.Web
                         context.Response.ContentType = "plain/text";
                         await context.Response.WriteAsync($"{path.Substring(1)}.{ThumbPrint}");
                     }
+                });
+            });
+
+            app.Map("/md", sub =>
+            {
+                sub.Run(context =>
+                {
+                    var referer = context.Request.Headers["Referer"].FirstOrDefault() ?? "";
+                    referer = referer.ToUpperInvariant();
+
+                    var targetUrl = context.Request.Query["r"].FirstOrDefault();
+                    var parts = targetUrl.Split('{', '}');
+
+                    var buffer = new StringBuilder();
+                    for (int i = 0; i < parts.Length; ++i)
+                    {
+                        if (i % 2 == 1)
+                        {
+                            var options = parts[i].Split('|');
+                            var branch = options
+                                .Where(b => referer.Contains($"/blob/{b}/") || referer.Contains($"/tree/{b}/") || referer.EndsWith($"/tree/{b}"))
+                                .FirstOrDefault() ?? options.FirstOrDefault();
+                            buffer.Append(branch ?? "master");
+                        }
+                        else
+                        {
+                            buffer.Append(parts[i]);
+                        }
+                    }
+
+                    context.Response.Redirect(buffer.ToString(), true);
+
+                    return Task.CompletedTask;
                 });
             });
 
