192bc8026fd94661897e8c8d33cac4940d41e5c9
Eddie Lin <fszlin@gmail.com>
2017-07-30 23:11:38 -0400

    unit tests for base64 conversion

---
+++ test/Certes.Tests/Jws/JwsConvertTests.cs
@@ -0,0 +1,24 @@
+﻿using Certes.Jws;
+using System.Linq;
+using Xunit;
+
+namespace Certes.Tests.Cli
+{
+    public class JwsConvertTests
+    {
+        [Fact]
+        public void CanConvertToBase64String()
+        {
+            var data = Enumerable
+                .Range(0, 1000)
+                .Select(i => (byte)i)
+                .ToArray();
+
+            var str = JwsConvert.ToBase64String(data);
+
+            var reverted = JwsConvert.FromBase64String(str);
+
+            Assert.Equal(data, reverted);
+        }
+    }
+}
