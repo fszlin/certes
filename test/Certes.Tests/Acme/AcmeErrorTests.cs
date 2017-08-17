e346b8dc08af899e690a25718012b86a1fac90e0
Eddie Lin <fszlin@gmail.com>
2017-08-17 10:06:30 -0400

    improve code coverage

---
+++ test/Certes.Tests/Acme/AcmeErrorTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using Xunit;
+
+namespace Certes.Acme
+{
+    public class AcmeErrorTests
+    {
+        [Fact]
+        public void CanGetSetProperties()
+        {
+            var model = new AcmeError();
+            model.VerifyGetterSetter(a => a.Detail, "error details");
+            model.VerifyGetterSetter(a => a.Status, HttpStatusCode.ExpectationFailed);
+            model.VerifyGetterSetter(a => a.Type, "error type");
+        }
+    }
+}
