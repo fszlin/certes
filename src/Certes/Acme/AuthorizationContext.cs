435872c4254cffeb5fa158d94d3fe66f70df0d5b
Eddie Lin <fszlin@gmail.com>
2017-08-22 23:45:29 -0400

    add authz flows

---
+++ src/Certes/Acme/AuthorizationContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Certes.Acme
+{
+    internal class AuthorizationContext
+    {
+        private readonly AcmeContext context;
+        private readonly IAccountContext account;
+        private readonly Uri location;
+
+        public AuthorizationContext(
+            AcmeContext context,
+            IAccountContext account,
+            Uri location)
+        {
+            this.context = context;
+            this.account = account;
+            this.location = location;
+        }
+    }
+}
--- src/Certes/Acme/OrderContext.cs
+++ src/Certes/Acme/OrderContext.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Certes.Acme.Resource;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Certes.Acme
 {
@@ -16,6 +20,27 @@ namespace Certes.Acme
             this.context = context;
             this.account = account;
             this.location = location;
+        }
+
+        /// <summary>
+        /// Resources this instance.
+        /// </summary>
+        /// <returns></returns>
+        public async Task<Order> Resource()
+        {
+            var (_, order, _) = await this.context.Get<Order>(location);
+            return order;
+        }
+
+        /// <summary>
+        /// Authorizationses this instance.
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IEnumerable<AuthorizationContext>> Authorizations()
+        {
+            var order = await this.Resource();
+            return order.Authorizations
+                .Select(a => new AuthorizationContext(this.context, this.account, a));
         }
     }
 }
