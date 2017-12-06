11243750c479333d899598543162673d2484638d
Eddie Lin <fszlin@gmail.com>
2017-12-06 13:01:27 -0500

    build script for vsts task

--- src/vsts/certes-ssl/certes-ssl.ts
+++ src/vsts/certes-ssl/certes-ssl.ts
@@ -1,19 +1,26 @@
+import path = require("path");
 import tl = require('vsts-task-lib/task');
-//npm install vsts-task-lib
 
-// Get task parameters
-let variable1: string = tl.getPathInput('variable1', false, true);
-let variable2: string = tl.getInput('variable2', true);
+export class CertesAuthzTask {
+    
+    public static checkIfAzurePythonSdkIsInstalled() {
+        return !!tl.which("az", true);
+    }
 
-async function run() {
-    try {
-        //do your actions
-        tl.debug('variable1:' +variable1)
-        tl.debug('variable2:' +variable2)
-        
-    } catch (err) {
-        tl.setResult(tl.TaskResult.Failed, err.message);
+    public static async runMain() {
+        try {
+            tl.setResourcePath(path.join(__dirname, "task.json"));
+
+            let indentifiers: string = tl.getInput('indentifiers', true);
+            tl.debug('indentifiers:' +indentifiers);
+        } catch (err) {
+            tl.setResult(tl.TaskResult.Failed, err.message);
+        }
     }
 }
 
-run();
+if (!CertesAuthzTask.checkIfAzurePythonSdkIsInstalled()) {
+    tl.setResult(tl.TaskResult.Failed, tl.loc("AzureSDKNotFound"));
+} else {
+    CertesAuthzTask.runMain();
+}
