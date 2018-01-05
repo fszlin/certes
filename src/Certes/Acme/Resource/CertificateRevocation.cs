94d3e9a2e37302a95de9f0dbd1298da6ced6fe36
Eddie Lin <fszlin@gmail.com>
2018-01-05 13:34:58 -0500

    [v2] add CertificateRevocation resource

---
+++ src/Certes/Acme/Resource/CertificateRevocation.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Certes.Acme.Resource
+{
+    internal class CertificateRevocation
+    {
+        [JsonProperty("certificate")]
+        public string Certificate { get; set; }
+
+        [JsonProperty("reason")]
+        public RevocationReason? Reason { get; set; }
+    }
+}
