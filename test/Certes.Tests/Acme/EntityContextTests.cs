36b95e993282b35a5a06c73f1d4ecd4f04d22094
Eddie Lin <fszlin@gmail.com>
2018-01-19 09:12:37 -0500

    +tests

---
+++ test/Certes.Tests/Acme/EntityContextTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Certes.Acme.Resource;
+using Moq;
+using Xunit;
+
+namespace Certes.Acme
+{
+    public class EntityContextTests
+    {
+        [Fact]
+        public async Task CanLoadResource()
+        {
+            var loc = new Uri("http://acme.d/acct/1");
+            var acct = new Account();
+
+            var httpMock = new Mock<IAcmeHttpClient>();
+            var ctxMock = new Mock<IAcmeContext>();
+            ctxMock.SetupGet(m => m.HttpClient).Returns(httpMock.Object);
+
+            httpMock.Setup(m => m.Get<Account>(loc)).ReturnsAsync(new AcmeHttpResponse<Account>(loc, acct, default, default));
+            var ctx = new EntityContext<Account>(ctxMock.Object, loc);
+
+            var res = await ctx.Resource();
+            Assert.Equal(acct, res);
+
+            loc = new Uri("http://acme.d/acct/2");
+            httpMock.Setup(m => m.Get<Account>(loc)).ReturnsAsync(new AcmeHttpResponse<Account>(loc, default, default, new AcmeError { Detail = "err" }));
+            ctx = new EntityContext<Account>(ctxMock.Object, loc);
+            await Assert.ThrowsAsync<Exception>(() => ctx.Resource());
+        }
+    }
+}
