b194431353a45105dd99e6ded6115e7e16828760
Eddie Lin <fszlin@gmail.com>
2018-01-23 22:01:52 -0500

    [v2] remove tls-sni-02

--- src/Certes/Acme/Resource/ChallengeTypes.cs
+++ src/Certes/Acme/Resource/ChallengeTypes.cs
@@ -11,11 +11,6 @@
         public const string Http01 = "http-01";
 
         /// <summary>
-        /// The tls-sni-02 challenge.
-        /// </summary>
-        public const string TlsSni02 = "tls-sni-02";
-
-        /// <summary>
         /// The dns-01 challenge.
         /// </summary>
         public const string Dns01 = "dns-01";
