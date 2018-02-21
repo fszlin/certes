f48eb77135a6fcd4ae601550dd6a7a0ad8f1adf9
Eddie Lin <5827855+fszlin@users.noreply.github.com>
2018-02-21 13:26:24 -0500

    enable parallel execution of integration tests (#38)

---
+++ test/Certes.Tests.Integration/AcmeContextIntegration.AccountDiscoveryTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+using static Certes.Helper;
+using static Certes.IntegrationHelper;
+
+namespace Certes
+{
+    public partial class AcmeContextIntegration
+    {
+        public class AccountDiscoveryTests : AcmeContextIntegration
+        {
+            public AccountDiscoveryTests(ITestOutputHelper output)
+                : base(output)
+            {
+            }
+
+            [Fact]
+            public async Task CanDiscoverAccountByKey()
+            {
+                var dirUri = await GetAcmeUriV2();
+
+                var ctx = new AcmeContext(dirUri, GetKeyV2(), GetAcmeHttpClient(dirUri));
+                var acct = await ctx.Account();
+
+                Assert.NotNull(acct.Location);
+
+                var res = await acct.Resource();
+            }
+        }
+    }
+}
