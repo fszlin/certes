7208fbe26f33045f870232b2d977d254a872814b
Eddie Lin <5827855+fszlin@users.noreply.github.com>
2018-02-22 01:42:17 -0500

    clean up tests

--- test/Certes.Tests/Cli/CliCoreTests.cs
+++ test/Certes.Tests/Cli/CliCoreTests.cs
@@ -2,9 +2,6 @@
 using System.Threading.Tasks;
 using Certes.Cli.Settings;
 using Moq;
-using NLog;
-using NLog.Config;
-using NLog.Targets;
 using Xunit;
 
 namespace Certes.Cli
@@ -15,15 +12,6 @@ namespace Certes.Cli
         [Fact]
         public async Task CanRunCommand()
         {
-            var config = new LoggingConfiguration();
-
-            config.LoggingRules.Add(
-                new LoggingRule("*", LogLevel.Debug, new ColoredConsoleTarget
-                {
-                    Layout = "${message}${onexception:${newline}${exception:format=tostring}}",
-                }));
-
-            LogManager.Configuration = config;
             var serverUri = new Uri("http://acme.com/d");
             var settingsMock = new Mock<IUserSettings>(MockBehavior.Strict);
             settingsMock.Setup(m => m.SetDefaultServer(serverUri)).Returns(Task.CompletedTask);
