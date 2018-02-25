e822440c13e622b9046b52a83ddef2a4ebbb6c2d
Eddie Lin <5827855+fszlin@users.noreply.github.com>
2018-02-25 00:33:51 -0500

    add 'cert pem' command

---
+++ src/Certes.Cli/Commands/CertificatePemCommand.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.CommandLine;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Certes.Acme.Resource;
+using Certes.Cli.Settings;
+using NLog;
+
+namespace Certes.Cli.Commands
+{
+    internal class CertificatePemCommand : CommandBase, ICliCommand
+    {
+        private const string CommandText = "pem";
+        private const string OrderIdParam = "order-id";
+        private const string OutOption = "out";
+        private static readonly ILogger logger = LogManager.GetLogger(nameof(CertificatePemCommand));
+
+        public CommandGroup Group { get; } = CommandGroup.Certificate;
+
+        public CertificatePemCommand(
+            IUserSettings userSettings,
+            IAcmeContextFactory contextFactory,
+            IFileUtil fileUtil)
+            : base(userSettings, contextFactory, fileUtil)
+        {
+        }
+
+        public ArgumentCommand<string> Define(ArgumentSyntax syntax)
+        {
+            var cmd = syntax.DefineCommand(CommandText, help: Strings.HelpCommandCertPem);
+
+            syntax
+                .DefineServerOption()
+                .DefineKeyOption()
+                .DefineOption(OutOption, help: Strings.HelpCertificateOut)
+                .DefineUriParameter(OrderIdParam, help: Strings.HelpOrderId);
+
+            return cmd;
+        }
+
+        public async Task<object> Execute(ArgumentSyntax syntax)
+        {
+            var (serverUri, key) = await ReadAccountKey(syntax, true, true);
+            var orderUri = syntax.GetParameter<Uri>(OrderIdParam, true);
+
+            logger.Debug("Downloading certificate from '{0}'.", serverUri);
+
+            var acme = ContextFactory.Create(serverUri, key);
+            var orderCtx = acme.Order(orderUri);
+            var order = await orderCtx.Resource();
+            if (order.Status != OrderStatus.Valid)
+            {
+                throw new Exception(
+                    string.Format(Strings.ErrorExportInvalidOrder, order.Status));
+            }
+
+            var cert = await orderCtx.Download();
+
+            var outPath = syntax.GetOption<string>(OutOption);
+            if (string.IsNullOrWhiteSpace(outPath))
+            {
+                return new
+                {
+                    location = order.Certificate,
+                    resource = new
+                    {
+                        certificate = cert.Certificate.ToDer(),
+                        issuers = cert.Issuers.Select(i => i.ToDer()).ToArray(),
+                    },
+                };
+            }
+            else
+            {
+                logger.Debug("Saving certificate to '{0}'.", outPath);
+
+                var buffer = new StringBuilder();
+                buffer.AppendLine(cert.Certificate.ToPem());
+                foreach (var issuer in cert.Issuers)
+                {
+                    buffer.AppendLine(issuer.ToPem());
+                }
+
+                await File.WriteAllText(outPath, buffer.ToString());
+
+                return new
+                {
+                    location = order.Certificate,
+                };
+
+            }
+        }
+    }
+}
---
+++ test/Certes.Tests/Cli/Commands/CertificatePemCommandTests.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.CommandLine;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Certes.Acme;
+using Certes.Acme.Resource;
+using Certes.Cli.Settings;
+using Moq;
+using Newtonsoft.Json;
+using Xunit;
+using static Certes.Acme.WellKnownServers;
+using static Certes.Cli.CliTestHelper;
+using static Certes.Helper;
+
+namespace Certes.Cli.Commands
+{
+    public class CertificatePemCommandTests
+    {
+        [Fact]
+        public async Task CanProcessCommand()
+        {
+            var orderLoc = new Uri("http://acme.com/o/1");
+            var certLoc = new Uri("http://acme.com/c/1");
+            var order = new Order
+            {
+                Certificate = certLoc,
+                Identifiers = new[] {
+                    new Identifier { Value = "*.a.com" },
+                    new Identifier { Value = "*.b.com" },
+                },
+                Status = OrderStatus.Valid,
+            };
+
+            var certChainContent = string.Join(
+                Environment.NewLine,
+                File.ReadAllText("./Data/leaf-cert.pem"),
+                File.ReadAllText("./Data/test-ca2.pem"),
+                File.ReadAllText("./Data/lets-encrypt-x3-cross-signed.pem"));
+            var certChain = new CertificateChain(certChainContent);
+
+            var settingsMock = new Mock<IUserSettings>(MockBehavior.Strict);
+            settingsMock.Setup(m => m.GetDefaultServer()).ReturnsAsync(LetsEncryptV2);
+            settingsMock.Setup(m => m.GetAccountKey(LetsEncryptV2)).ReturnsAsync(GetKeyV2());
+
+            var orderMock = new Mock<IOrderContext>(MockBehavior.Strict);
+            orderMock.Setup(m => m.Resource()).ReturnsAsync(order);
+            orderMock.Setup(m => m.Download()).ReturnsAsync(certChain);
+
+            var ctxMock = new Mock<IAcmeContext>(MockBehavior.Strict);
+            ctxMock.Setup(m => m.GetDirectory()).ReturnsAsync(MockDirectoryV2);
+            ctxMock.Setup(m => m.Order(orderLoc)).Returns(orderMock.Object);
+
+            var fileMock = new Mock<IFileUtil>(MockBehavior.Strict);
+
+            var cmd = new CertificatePemCommand(
+                settingsMock.Object, MakeFactory(ctxMock), fileMock.Object);
+
+            var syntax = DefineCommand($"pem {orderLoc}");
+            var ret = await cmd.Execute(syntax);
+            Assert.Equal(
+                JsonConvert.SerializeObject(new
+                {
+                    location = certLoc,
+                    resource = new
+                    {
+                        certificate = certChain.Certificate.ToDer(),
+                        issuers = certChain.Issuers.Select(i => i.ToDer()),
+                    },
+                }),
+                JsonConvert.SerializeObject(ret));
+
+            orderMock.Verify(m => m.Download(), Times.Once);
+
+            var outPath = "./cert.pem";
+            string saved = null;
+            fileMock.Setup(m => m.WriteAllText(outPath, It.IsAny<string>()))
+                .Callback((string path, string text) => saved = text)
+                .Returns(Task.CompletedTask);
+            syntax = DefineCommand($"pem --out {outPath} {orderLoc}");
+            ret = await cmd.Execute(syntax);
+            Assert.Equal(
+                JsonConvert.SerializeObject(new
+                {
+                    location = certLoc,
+                }),
+                JsonConvert.SerializeObject(ret));
+
+            fileMock.Verify(m => m.WriteAllText(outPath, It.IsAny<string>()), Times.Once);
+            Assert.Equal(certChainContent.Replace(Environment.NewLine, ""), saved.Replace(Environment.NewLine, ""));
+
+            order.Status = OrderStatus.Invalid;
+            syntax = DefineCommand($"pem {orderLoc}");
+            await Assert.ThrowsAsync<Exception>(() => cmd.Execute(syntax));
+        }
+
+        [Fact]
+        public void CanDefineCommand()
+        {
+            var args = $"pem http://acme.com/o/1 --server {LetsEncryptStagingV2}";
+            var syntax = DefineCommand(args);
+
+            Assert.Equal("pem", syntax.ActiveCommand.Value);
+            ValidateOption(syntax, "server", LetsEncryptStagingV2);
+            ValidateParameter(syntax, "order-id", new Uri("http://acme.com/o/1"));
+
+            syntax = DefineCommand("noop");
+            Assert.NotEqual("pem", syntax.ActiveCommand.Value);
+        }
+
+        private static ArgumentSyntax DefineCommand(string args)
+        {
+            var cmd = new CertificatePemCommand(
+                new UserSettings(new FileUtilImpl()), MakeFactory(null), new FileUtilImpl());
+            return ArgumentSyntax.Parse(args.Split(' '), syntax =>
+            {
+                syntax.HandleErrors = false;
+                syntax.DefineCommand("noop");
+                cmd.Define(syntax);
+            });
+        }
+    }
+}
