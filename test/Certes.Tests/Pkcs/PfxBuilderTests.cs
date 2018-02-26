0bd167309c751fc8efe2906b1eef21e3b0be4a08
Eddie Lin <5827855+fszlin@users.noreply.github.com>
2018-02-26 09:51:14 -0500

    +tests

--- test/Certes.Tests/Pkcs/PfxBuilderTests.cs
+++ test/Certes.Tests/Pkcs/PfxBuilderTests.cs
@@ -23,6 +23,20 @@ namespace Certes.Pkcs
             pfxBuilder.AddIssuer(File.ReadAllBytes("./Data/test-root.pem"));
             var pfx = pfxBuilder.Build("my-cert", "abcd1234");
         }
+        [Theory]
+        [InlineData(KeyAlgorithm.RS256)]
+        [InlineData(KeyAlgorithm.ES256)]
+        [InlineData(KeyAlgorithm.ES384)]
+        [InlineData(KeyAlgorithm.ES512)]
+        public void CanCreatePfxWithoutChain(KeyAlgorithm alog)
+        {
+            var leafCert = File.ReadAllText("./Data/leaf-cert.pem");
+
+            var pfxBuilder = new PfxBuilder(
+                Encoding.UTF8.GetBytes(leafCert), KeyFactory.NewKey(alog));
+            pfxBuilder.FullChain = false;
+            var pfx = pfxBuilder.Build("my-cert", "abcd1234");
+        }
 
         [Theory]
         [InlineData(KeyAlgorithm.RS256)]
