31c3a4d9bb85f025fcae800d689af28a23d8c2da
Eddie Lin <5827855+fszlin@users.noreply.github.com>
2018-02-26 20:48:02 -0500

    add DnsClientFactory

---
+++ src/Certes.Cli/DnsClientFactory.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Azure.Management.Dns.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
+
+namespace Certes.Cli
+{
+    public class DnsClientFactory : IDnsClientFactory
+    {
+        public IDnsManagementClient Create(AzureCredentials credentials)
+            => new DnsManagementClient(credentials);
+    }
+}
---
+++ src/Certes.Cli/IDnsClientFactory.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Azure.Management.Dns.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
+
+namespace Certes.Cli
+{
+    internal interface IDnsClientFactory
+    {
+        IDnsManagementClient Create(AzureCredentials credentials);
+    }
+}
