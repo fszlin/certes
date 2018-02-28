f7b2ad2fa58ed9f9c1156c8d34b997a6a7916358
Eddie Lin <5827855+fszlin@users.noreply.github.com>
2018-02-28 00:40:14 -0500

    +tests

--- src/Certes.Cli/Settings/AcmeSettings.cs
+++ src/Certes.Cli/Settings/AcmeSettings.cs
@@ -5,7 +5,6 @@ namespace Certes.Cli.Settings
     internal class AcmeSettings
     {
         public Uri ServerUri { get; set; }
-        public string AccountKey { get; set; }
         public byte[] Key { get; set; }
     }
 }
