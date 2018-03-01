25b5c3813645ecc12c319fde2753deefd9973da6
Eddie Lin <5827855+fszlin@users.noreply.github.com>
2018-03-01 14:30:56 -0500

    update version patching for appveyor

--- AssemblyInfo.Shared.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0.0")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
---
+++ misc/AssemblyInfo.Shared.cs
@@ -0,0 +1,5 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
