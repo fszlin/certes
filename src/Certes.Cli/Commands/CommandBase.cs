34967624134cea742bb155af6fafe1b3bc8676fa
Eddie Lin <5827855+fszlin@users.noreply.github.com>
2018-03-06 23:30:46 -0500

    Revert "accept DER account key via CLI"
    This reverts commit 69f44507e12e95e479d8d2afc49bcb961df78b33.

--- src/Certes.Cli/Commands/CommandBase.cs
+++ src/Certes.Cli/Commands/CommandBase.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.CommandLine;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Certes.Cli.Settings;
 using NLog;
@@ -9,8 +8,6 @@ namespace Certes.Cli.Commands
 {
     internal abstract class CommandBase
     {
-        private static Regex Base64Regex = new Regex("^[a-zA-Z0-9+/]+={0,2}$", RegexOptions.Compiled);
-
         private static readonly ILogger logger = LogManager.GetLogger(nameof(CommandBase));
         protected IFileUtil File { get; private set; }
         protected IUserSettings UserSettings { get; private set; }
@@ -32,21 +29,12 @@ namespace Certes.Cli.Commands
             var serverUri = syntax.GetServerOption() ??
                 await UserSettings.GetDefaultServer();
 
-            var keyValue = syntax.GetKeyOption();
-            if (!string.IsNullOrWhiteSpace(keyValue))
+            var keyPath = syntax.GetKeyOption();
+            if (!string.IsNullOrWhiteSpace(keyPath))
             {
-                var likeBase64 = Base64Regex.IsMatch(keyValue);
-                if (likeBase64)
-                {
-                    var der = Convert.FromBase64String(keyValue);
-                    return (serverUri, KeyFactory.FromDer(der));
-                }
-                else
-                {
-                    logger.Debug("Load account key form '{0}'.", keyValue);
-                    var pem = await File.ReadAllText(keyValue);
-                    return (serverUri, KeyFactory.FromPem(pem));
-                }
+                logger.Debug("Load account key form '{0}'.", keyPath);
+                var pem = await File.ReadAllText(keyPath);
+                return (serverUri, KeyFactory.FromPem(pem));
             }
 
             var key = fallbackToSettings ?
