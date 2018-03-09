129162c1a275a6c0c348e03ac09272ad0420b8ed
Eddie Lin <5827855+fszlin@users.noreply.github.com>
2018-03-08 22:20:15 -0500

    +tests

---
+++ test/Certes.Tests/Cli/ArgumentSyntaxExtensionsTests.cs
@@ -0,0 +1,22 @@
+﻿using System.CommandLine;
+using Xunit;
+
+namespace Certes.Cli
+{
+    public class ArgumentSyntaxExtensionsTests
+    {
+        [Fact]
+        public void CanGetOption()
+        {
+            var args = new[] { "--my-opt", "1" };
+            var s = ArgumentSyntax.Parse(args, syntax =>
+            {
+                syntax.HandleErrors = false;
+                syntax.DefineOption("my-opt", "0");
+            });
+
+            Assert.Equal("1", s.GetOption<string>("my-opt", true));
+            Assert.Throws<ArgumentSyntaxException>(() => s.GetOption<string>("another-opt", true));
+        }
+    }
+}
