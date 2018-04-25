4c1ccdb0d6d84b4da728fbbd9972d1f02bf991de
Eddie Lin <5827855+fszlin@users.noreply.github.com>
2018-04-24 22:48:42 -0400

    +tests

---
+++ test/Certes.Tests/Acme/AcmeRequestExceptionTests.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+using Xunit;
+
+#if !NETCOREAPP1_0
+using System.Runtime.Serialization.Formatters.Binary;
+#endif
+
+namespace Certes.Acme
+{
+    public class AcmeRequestExceptionTests
+    {
+        [Fact]
+        public void CanCreateException()
+        {
+            var ex = new AcmeRequestException();
+        }
+
+        [Fact]
+        public void CanCreateExceptionWithMessage()
+        {
+            var ex = new AcmeRequestException("certes");
+            Assert.Equal("certes", ex.Message);
+        }
+
+        [Fact]
+        public void CanCreateExceptionWithInnerException()
+        {
+            var inner = new AcmeException();
+            var ex = new AcmeRequestException("certes", inner);
+            Assert.Equal("certes", ex.Message);
+            Assert.Equal(inner, ex.InnerException);
+        }
+
+#if !NETCOREAPP1_0
+        [Fact]
+        public void CanSerialize()
+        {
+            var error = new AcmeError { Detail = "error" };
+            var ex = new AcmeRequestException("certes", error);
+            
+            var serializer = new BinaryFormatter();
+
+            using (var buffer = new MemoryStream())
+            {
+                serializer.Serialize(buffer, ex);
+
+                buffer.Seek(0, SeekOrigin.Begin);
+                var deserialized = (AcmeRequestException)serializer.Deserialize(buffer);
+
+                Assert.NotNull(deserialized.Error.Detail);
+            }
+        }
+#endif
+    }
+}
