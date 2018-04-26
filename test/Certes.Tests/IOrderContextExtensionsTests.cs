099414cdb6a6e69d2a58facbaf4cc910dd5455d9
Eddie Lin <5827855+fszlin@users.noreply.github.com>
2018-04-25 23:51:45 -0400

    +tests

--- test/Certes.Tests/IOrderContextExtensionsTests.cs
+++ test/Certes.Tests/IOrderContextExtensionsTests.cs
@@ -11,7 +11,51 @@ namespace Certes
     public class IOrderContextExtensionsTests
     {
         [Fact]
-        public async Task CanGenerateCertificate()
+        public async Task CanGenerateCertificateWhenOrderReady()
+        {
+            var pem = File.ReadAllText("./Data/cert-es256.pem");
+
+            var orderCtxMock = new Mock<IOrderContext>();
+            orderCtxMock.Setup(m => m.Download()).ReturnsAsync(new CertificateChain(pem));
+            orderCtxMock.Setup(m => m.Resource()).ReturnsAsync(new Order
+            {
+                Identifiers = new[] {
+                    new Identifier { Value = "www.certes.com", Type = IdentifierType.Dns },
+                },
+                Status = OrderStatus.Ready,
+            });
+            orderCtxMock.Setup(m => m.Finalize(It.IsAny<byte[]>()))
+                .ReturnsAsync(new Order
+                {
+                    Identifiers = new[] {
+                        new Identifier { Value = "www.certes.com", Type = IdentifierType.Dns },
+                    },
+                    Status = OrderStatus.Valid,
+                });
+
+            var key = KeyFactory.NewKey(KeyAlgorithm.RS256);
+            var certInfo = await orderCtxMock.Object.Generate(new CsrInfo
+            {
+                CountryName = "C",
+                CommonName = "www.certes.com",
+            }, key);
+
+            Assert.Equal(
+                pem.Where(c => !char.IsWhiteSpace(c)),
+                certInfo.Certificate.ToPem().Where(c => !char.IsWhiteSpace(c)));
+
+            var certInfoNoCn = await orderCtxMock.Object.Generate(new CsrInfo
+            {
+                CountryName = "C",
+            }, key);
+
+            Assert.Equal(
+                pem.Where(c => !char.IsWhiteSpace(c)),
+                certInfoNoCn.Certificate.ToPem().Where(c => !char.IsWhiteSpace(c)));
+        }
+
+        [Fact]
+        public async Task CanGenerateCertificateWhenOrderPending()
         {
             var pem = File.ReadAllText("./Data/cert-es256.pem");
 
