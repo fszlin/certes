9799d4faba1eec09a53cfa4a475b035d78944b47
Eddie Lin <5827855+fszlin@users.noreply.github.com>
2018-06-20 09:04:14 -0400

    reseed doc pages

--- src/Certes.Cli/Properties/AssemblyInfo.cs
+++
@@ -1,3 +0,0 @@
-﻿using System.Runtime.InteropServices;
-
-[assembly: Guid("23fd2074-1b0f-495a-831e-7a0d645cdf88")]
--- src/Certes/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: Guid("5f6267a0-a6c5-4d1e-b7ff-f8f8d09d8588")]
-[assembly: InternalsVisibleTo("Certes.Tests")]
