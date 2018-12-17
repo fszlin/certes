d8ccdf7b89e60315ce1c4e4f5875c8a95606cd4c
Eddie Lin <5827855+fszlin@users.noreply.github.com>
2018-12-16 21:33:57 -0500

    update changelog

--- src/Certes/Json/JsonUtil.cs
+++ src/Certes/Json/JsonUtil.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using Certes.Jws;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 
 namespace Certes.Json
@@ -29,31 +24,6 @@ namespace Certes.Json
             };
 
             return jsonSettings;
-        }
-    }
-
-    /// <summary>
-    /// JSON contract resolver supports ordering JWK properties.
-    /// </summary>
-    /// <seealso cref="Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver" />
-    internal sealed class ContractResolver : CamelCasePropertyNamesContractResolver
-    {
-        /// <summary>
-        /// Creates properties for the given <see cref="T:Newtonsoft.Json.Serialization.JsonContract" />.
-        /// </summary>
-        /// <param name="type">The type to create properties for.</param>
-        /// <param name="memberSerialization">The member serialization mode for the type.</param>
-        /// <returns>
-        /// Properties for the given <see cref="T:Newtonsoft.Json.Serialization.JsonContract" />.
-        /// </returns>
-        protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization)
-        {
-            if (typeof(JsonWebKey).GetTypeInfo().IsAssignableFrom(type.GetTypeInfo()))
-            {
-                return base.CreateProperties(type, memberSerialization).OrderBy(p => p.PropertyName).ToArray();
-            }
-
-            return base.CreateProperties(type, memberSerialization);
         }
     }
 }
