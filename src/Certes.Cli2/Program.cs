a9db32ae9fc28a92db1478d753eae842d3401426
Eddie Lin <5827855+fszlin@users.noreply.github.com>
2019-11-03 16:40:34 -0500

    update tests

--- src/Certes.Cli2/Program.cs
+++ src/Certes.Cli2/Program.cs
@@ -2,11 +2,11 @@
 
 namespace Certes.Cli2
 {
-    class Program
+    class Program2
     {
-        static void Main(string[] args)
+        static void Main2(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
         }
     }
 }
