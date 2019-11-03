fcb9fb0d6f1eae0d74e2a459d72236f05778aca5
Eddie Lin <5827855+fszlin@users.noreply.github.com>
2019-11-03 12:29:09 -0500

    migrate tests

---
+++ test/Certes.Tests2/CsrInfoTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace Certes
+{
+    public class CsrInfoTests
+    {
+        [Fact]
+        public void CanSetInfo()
+        {
+            var csr = new CsrInfo
+            {
+                CommonName = "CommonName",
+                Locality = "Locality",
+                CountryName = "CountryName",
+                Organization = "Organization",
+                OrganizationUnit = "OrganizationUnit",
+                State = "State",
+            };
+
+            Assert.Equal("CommonName", csr.Fields.Single(f => f.name == "CN").value);
+            Assert.Equal("Locality", csr.Fields.Single(f => f.name == "L").value);
+            Assert.Equal("CountryName", csr.Fields.Single(f => f.name == "C").value);
+            Assert.Equal("Organization", csr.Fields.Single(f => f.name == "O").value);
+            Assert.Equal("OrganizationUnit", csr.Fields.Single(f => f.name == "OU").value);
+            Assert.Equal("State", csr.Fields.Single(f => f.name == "ST").value);
+        }
+    }
+}
---
+++ test/Certes.Tests2/Pkcs/SignatureAlgorithmTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xunit;
+
+namespace Certes.Pkcs
+{
+    public class SignatureAlgorithmTests
+    {
+        [Fact]
+        public void ToJwsAlgorithmInvalid()
+        {
+            Assert.Throws<ArgumentException>(() =>
+                ((KeyAlgorithm)1000).ToJwsAlgorithm());
+        }
+
+        [Fact]
+        public void ToPkcsObjectIdInvalid()
+        {
+            Assert.Null(
+                ((KeyAlgorithm)1000).ToPkcsObjectId());
+        }
+    }
+}
