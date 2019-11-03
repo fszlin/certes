a6a4bb6d7a4c2c137dd8c5b59747ac44de901a37
Eddie Lin <5827855+fszlin@users.noreply.github.com>
2019-11-03 14:44:10 -0500

    Revert "update tests"
    This reverts commit 193295d31076dd57e781ab92f966339af53cd2e4.

---
+++ test/Certes.Tests2/Acme/Resource/AuthorizationTests.cs
@@ -0,0 +1,21 @@
+﻿using Certes.Acme.Resource;
+using System;
+using Xunit;
+
+namespace Certes.Tests.Acme.Resource
+{
+    public class AuthorizationTests
+    {
+        [Fact]
+        public void CanGetSetProperties()
+        {
+            var authz = new Authorization();
+            authz.VerifyGetterSetter(a => a.Status, AuthorizationStatus.Deactivated);
+            authz.VerifyGetterSetter(a => a.Challenges, new[] { new Challenge() });
+            authz.VerifyGetterSetter(a => a.Expires, DateTimeOffset.Now);
+            authz.VerifyGetterSetter(a => a.Identifier, new Identifier());
+            authz.VerifyGetterSetter(a => a.Wildcard, true);
+            authz.VerifyGetterSetter(a => a.Scope, new Uri("http://certes.is.working"));
+        }
+    }
+}
